4caea85dccfba57fa09af2822564ca8fa4eb80f0
0xK3rn3l-dev <emailbycat222@gmail.com>
2026-03-25 22:21:17 +0300

    added gameScreen v00

--- levels/level_00.py
+++ levels/level_00.py
@@ -1,4 +1,13 @@
-
 class Level_00:
     def __init__(self):
-        pass
+        self.name = "Forest"
+        self.speed = 5
+        self.snake_start = [(500, 600)]
+        self.foods = [(200, 200), (600, 400), (400, 100)]
+        self.walls = [
+            (0, 0, 800, 20),      # верхняя стена
+            (0, 580, 800, 20),    # нижняя стена
+            (0, 0, 20, 600),      # левая стена
+            (780, 0, 20, 600)     # правая стена
+        ]
+        self.background_color = (10, 50, 10) #Green
